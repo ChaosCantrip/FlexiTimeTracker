8fe1ec6169970152c893b13861f550ce64749693
ChaosCantrip <dev@chaoscantrip.com>
2026-04-19 23:54:36 +0100

    Updated Footer

--- src/app/(website)/Footer.tsx
+++ src/app/(website)/Footer.tsx
@@ -1,10 +1,14 @@
 import styles from "./Footer.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
+const GITHUB_ICON = <FontAwesomeIcon icon={faGithub} />
 
 export default function Footer() 
 {
     return (
         <footer className={styles.footer}>
-            <p>Created by <a href="https://github.com/ChaosCantrip">ChaosCantrip</a></p>
+            <p><a href="https://github.com/ChaosCantrip/FlexiTimeTracker">{GITHUB_ICON} FlexiTimeTracker</a> created by <a href="https://github.com/ChaosCantrip">{GITHUB_ICON} ChaosCantrip</a></p>
         </footer>
     )
 }
